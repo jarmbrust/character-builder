31e682121a491014213349de3f069fe1d331d62e
James Armbrust <james.armbrust@gmail.com>
2018-04-28 17:56:42 -0600

    inprogress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      str: 8,
+      dex: 8,
+      con: 8,
+      int: 8,
+      wis: 8,
+      chr: 8,
+      cost: {
+				8: 0,
+				9: 1,
+				10: 2,
+				11: 3,
+				12: 4,
+				13: 5,
+				14: 7,
+				15: 9
+			},
+      totalPoints: 27
+    };
+
+    this.pointCost = this.pointCost.bind(this);
+    this.modifyTotal = this.modifyTotal.bind(this);
+  }
+
+  modifyTotal(points) {
+    const newTotal = this.state.totalPoints + points;
+    console.log(newTotal);
+    this.setState({
+      totalPoints: newTotal
+    });
+  }
+
+  pointCost(ability) {
+    const cost = this.state.cost[ability];
+    //this.modifyTotal(cost);
+    return cost;
+  }
+
   render() {
+
+		const {
+			str,
+			dex,
+      con,
+			int,
+			wis,
+      chr
+		} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Character Builder</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <AbilityScore
+            ability="Strength"
+            val={str}
+						cost={this.pointCost(str)}
+          >
+
+          </AbilityScore>
+					<AbilityScore
+            ability="Dexterity"
+            val={dex}
+            cost={this.pointCost(dex)}
+          />
+					<AbilityScore
+            ability="Constitution"
+            val={con}
+            cost={this.pointCost(con)}
+          />
+					<AbilityScore
+            ability="Intelligence"
+            val={int}
+            cost={this.pointCost(int)}
+          />
+					<AbilityScore
+            ability="Wisdom"
+            val={wis}
+            cost={this.pointCost(wis)}
+          />
+					<AbilityScore
+            ability="Charisma"
+            val={chr}
+            cost={this.pointCost(chr)}
+          />
+        </div>
       </div>
     );
   }
 }
+
+const AbilityScore = ({ ability, val, cost }) => {
+  const bonus = Math.floor((val - 10)/2);
+  return (
+    <div className="ability-score">
+      <span>{ability}</span>
+      <span>{val}</span>
+      <span>{bonus}</span>
+			<span>{cost}</span>
+    </div>
+  )
+};
 
 export default App;
